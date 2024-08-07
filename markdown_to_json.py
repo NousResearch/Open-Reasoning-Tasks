7ccc27363dc19aa979c14688c4ca3f4a0519eb6d
Aaditya Ura (looking for PhD Fall’24) <aadityaura@gmail.com>
2024-08-07 09:17:36 +0530

    Update markdown_to_json.py

--- markdown_to_json.py
+++ markdown_to_json.py
@@ -31,7 +31,10 @@ def process_tasks(tasks_folder, json_folder):
                 
                 # Write the JSON content to file
                 with open(json_path, 'w', encoding='utf-8') as json_file:
-                    json.dump(json_content, json_file, indent=2, ensure_ascii=False)
+                    json.dump(json_content, json_file, indent=2, ensure_ascii=False, separators=(',', ': '), sort_keys=True)
+                    json_file.write('\n')  # Add a final newline
+
+                
                 
                 print(f"Successfully converted and saved {json_file}")
             except MarkdownParsingError as e:
